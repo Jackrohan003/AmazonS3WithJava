230ac249003f48b23ae9f57f2f596d0adde70e8c
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-02-06 19:28:54 +0530

    Amazon S3 Service using Java

---
+++ AmazonS3WithJava/src/main/java/com/aws/s3/constants/CommonConstants.java
@@ -0,0 +1,24 @@
+package com.aws.s3.constants;
+
+public class CommonConstants {
+	
+	public static String ACCESS_KEY_ID = "ACCESS_KEY_ID";
+	
+	public static String ACCESS_SEC_KEY = "ACCESS_SEC_KEY";
+	
+	public static String BUCKET_NAME = "amazoncodedeploybucket";
+	
+	public static String FOLDER_NAME = "storeFiles";
+	
+	public static String FILE_NAME = "Invoice_May.pdf";
+	
+	public static String FILE_PATH = "C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\Invoice_May.pdf";
+	
+	public static String BUCKET_FILE_PATH = "storeFiles/Invoice_May.pdf";
+	
+	public static String LOCAL_DOWNLOAD_PATH = "C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\Invoice_May_new.pdf";
+	
+	public static final String SUFFIX = "/";
+	
+
+}
